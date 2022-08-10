f2590652140e3876e3fde20a4bec67d122d31518
pizihao <2335715300@qq.com>
2022-08-10 19:43:06 +0800

    因类调整导致的方法引用错误

--- src/main/java/com/deep/jsr269/attribute/ArrayAttribute.java
+++ src/main/java/com/deep/jsr269/attribute/ArrayAttribute.java
@@ -1,6 +1,5 @@
 package com.deep.jsr269.attribute;
 
-import com.deep.jsr269.ProcessorUtil;
 import com.sun.tools.javac.code.Attribute;
 import com.sun.tools.javac.code.Symbol;
 import com.sun.tools.javac.tree.JCTree;
